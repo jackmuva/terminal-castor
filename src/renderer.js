f729fae5e7afbbecfee3a27cb4925f4cdd3b4a21
Jack Mu <jackmu@umich.edu>
2025-06-15 10:04:24 -0700

    made sure the terminal container had the right tw classes; dont forget to re-run the tw compile

--- src/renderer.js
+++ src/renderer.js
@@ -12,20 +12,15 @@ const {
 	themes
 } = require('./styles/terminal-theme.js');
 
-// 🎨 Choose your theme here! 
-// Available themes: currentTheme, draculaTheme, monokaiTheme, solarizedDarkTheme, oneDarkTheme, nordTheme, gruvboxDarkTheme
 const selectedTheme = draculaTheme;
-
 const term = new Terminal({
 	theme: selectedTheme
 });
-
 const fitAddon = new FitAddon();
 term.loadAddon(fitAddon);
 
 term.open(document.getElementById('terminal'));
 
-// Function to fit terminal and update pty size
 const fitAndResize = () => {
 	fitAddon.fit();
 	const dims = fitAddon.proposeDimensions();
@@ -35,7 +30,6 @@ const fitAndResize = () => {
 	}
 };
 
-// Fit terminal to container initially
 setTimeout(() => {
 	fitAndResize();
 }, 100);
@@ -48,14 +42,12 @@ term.onData(e => {
 	ipc.send("terminal.keystroke", e);
 });
 
-// Handle window resize events
 window.addEventListener('resize', () => {
 	setTimeout(() => {
 		fitAndResize();
 	}, 100);
 });
 
-// Handle explicit resize requests from main process
 ipc.on("terminal.forceResize", () => {
 	setTimeout(() => {
 		fitAndResize();
