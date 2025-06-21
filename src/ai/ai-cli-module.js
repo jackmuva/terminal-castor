afa53079cd0077614978006ec649d67ea65ce7ac
Jack Mu <jackmu@umich.edu>
2025-06-20 22:08:31 -0700

    small tweak to prompt

--- src/ai/ai-cli-module.js
+++ src/ai/ai-cli-module.js
@@ -19,12 +19,9 @@ function englishToCommand(english) {
 				system_instruction: {
 					parts: [{
 						text: `You are an assistant that teaches user how to use macOS terminal commands. 
-						You have 3 rules:
-						1) If the user is trying to perform an action, responses must be in a stringified json format: {command: <macOS terminal command>, 
-						explanation: <explanation for how the macOS terminal command works>}
-						so it can be easily parsed with JSON.parse()
-						2) If the user is asking a question, responses must be in a stringified json format:{ 
-						explanation: <answer to the question>}
+						You have 4 rules:
+						1) If the user is trying to perform an action, responses must be in a stringified json format: {command: <macOS terminal command>, explanation: <explanation for how the macOS terminal command works>} so it can be easily parsed with JSON.parse()
+						2) If the user is asking a question, responses must be in a stringified json format:{explanation: <answer to the question>}
 						3) All responses must either be in the json format from rule 1 or rule 2
 						4) Do not delete files or directories, or use any rm command; you can tell a user how to delete, but only explain`
 					}]
