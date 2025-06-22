6753a837bdd081eef3ab1652df96fefcd17f725c
Jack Mu <jackmu@umich.edu>
2025-06-22 14:10:33 -0700

    everything working together; need to figure out my lsp and test with wrangler deploy

--- electron-castor/src/ai/ai-cli-module.js
+++ electron-castor/src/ai/ai-cli-module.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 /** @returns {Promise<AiResponse>} */
 function englishToCommand(english) {
 	return new Promise((resolve, reject) => {
-		fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${process.env.GEMINI_API_KEY}`, {
+		fetch(process.env.CF_API + "/api/chat", {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({
@@ -113,7 +113,7 @@ async function listCommandToolCall(english, cliToolName) {
 	return listHelp(cliToolName)
 		.then((cliToolContext) => {
 			return new Promise((resolve, reject) => {
-				fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${process.env.GEMINI_API_KEY}`, {
+				fetch(process.env.CF_API + "/api/chat", {
 					method: "POST",
 					headers: { "Content-Type": "application/json" },
 					body: JSON.stringify({
